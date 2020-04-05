bf3e180ad2f0f42e011ca216aa3a79e1faab890f
hvxzcb <dexdot@MacBook-Pro-Kamil.local>
2020-04-05 12:40:36 +0300

    Orders popups styles

--- src/js/order-popup.js
+++ src/js/order-popup.js
@@ -1,75 +1,81 @@
+const closePopups = () => {
+  const popups = Array.from(
+    document.querySelectorAll('.order-cell-popup, .orders-item-popup')
+  );
+
+  popups.forEach(popup => {
+    popup.classList.remove('active');
+  });
+};
+
 window.addEventListener('DOMContentLoaded', () => {
   const buttons = document.querySelectorAll('.js-order-popup');
+  const popup = document.querySelector('.order-cell-popup');
+  const contentHeight = popup.offsetHeight;
+  const contentWidth = popup.offsetWidth;
+  const closeBtn = popup.querySelector('.popup__close');
+  closeBtn.addEventListener('click', closePopups);
 
   buttons.forEach(btn => {
-    const parent = btn.closest('.cell');
-    const popup = parent.querySelector('.order-cell-popup');
-    const closeBtn = popup.querySelector('.popup__close');
-
-    const contentHeight = popup.offsetHeight;
-    const contentWidth = popup.offsetWidth;
+    const cell = btn.closest('.cell');
 
     btn.addEventListener('click', () => {
+      closePopups();
+
+      const top = cell.offsetTop;
+      const left = cell.offsetLeft;
+
       // Top / Bottom
-      if (parent.offsetTop > contentHeight) {
-        popup.style.top = 'unset';
-        popup.style.bottom = 0;
-      } else if (parent.offsetTop > contentHeight / 2) {
-        popup.style.bottom = 'unset';
-        popup.style.top = `-${contentHeight / 2}px`;
+      if (top > contentHeight) {
+        popup.style.top = `${top - contentHeight}px`;
+      } else if (top > contentHeight / 2) {
+        popup.style.top = `${top - contentHeight / 2}px`;
       } else {
-        popup.style.bottom = 'unset';
-        popup.style.top = 0;
+        popup.style.top = `${top + cell.offsetHeight}px`;
       }
 
       // Left / Right
-      if (parent.offsetLeft > contentWidth) {
-        popup.style.left = 'unset';
-        popup.style.right = 0;
+      if (left > contentWidth) {
+        popup.style.left = `${left - contentWidth + cell.offsetWidth}px`;
       } else {
-        popup.style.right = 'unset';
-        popup.style.left = 0;
+        popup.style.left = `${left}px`;
       }
 
       popup.classList.add('active');
-    });
-
-    closeBtn.addEventListener('click', () => {
-      popup.classList.remove('active');
     });
   });
 });
 
 window.addEventListener('DOMContentLoaded', () => {
   const buttons = document.querySelectorAll('.js-show-order-popup');
+  const popup = document.querySelector('.orders-item-popup');
+  const contentHeight = popup.offsetHeight;
+  const contentWidth = popup.offsetWidth;
+  const closeBtn = popup.querySelector('.popup__close');
+  closeBtn.addEventListener('click', closePopups);
 
   buttons.forEach(btn => {
-    const parent = btn.closest('li');
-    const popup = parent.querySelector('.orders-item-popup');
-    const closeBtn = popup.querySelector('.popup__close');
-
-    const contentHeight = popup.offsetHeight;
-    const contentWidth = popup.offsetWidth;
-
     btn.addEventListener('click', () => {
-      popup.style.bottom = 'unset';
-      popup.style.right = 'unset';
-      const top = btn.offsetTop + 16;
+      closePopups();
+
+      const top = btn.offsetTop;
       const left = btn.offsetLeft;
 
       // Top / Bottom
       if (top > contentHeight) {
-        popup.style.top = `${top - contentHeight - 16}px`;
+        popup.style.top = `${top - contentHeight - 8}px`;
       } else {
-        popup.style.top = `${top + btn.offsetHeight}px`;
+        popup.style.top = `${top + btn.offsetHeight + 8}px`;
       }
 
-      popup.style.left = `${left}px`;
-      popup.classList.add('active');
-    });
+      // Left / Right
+      if (left > contentWidth) {
+        popup.style.left = `${left - btn.offsetWidth}px`;
+      } else {
+        popup.style.left = `${left}px`;
+      }
 
-    closeBtn.addEventListener('click', () => {
-      popup.classList.remove('active');
+      popup.classList.add('active');
     });
   });
 });
