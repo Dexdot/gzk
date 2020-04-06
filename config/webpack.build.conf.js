e45aeb3c87eb58a8eac7637f79645e72278e45eb
hvxzcb <dexdot@MacBook-Pro-Kamil.local>
2020-04-06 11:51:49 +0300

    HTML Pretty

--- config/webpack.build.conf.js
+++ config/webpack.build.conf.js
@@ -1,9 +1,9 @@
-const merge = require("webpack-merge");
-const { CleanWebpackPlugin } = require("clean-webpack-plugin");
-const baseWebpackConfig = require("./webpack.base.conf");
+const merge = require('webpack-merge');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const baseWebpackConfig = require('./webpack.base.conf');
 
 const buildWebpackConfig = merge(baseWebpackConfig, {
-  mode: "production",
+  mode: 'production',
   plugins: [new CleanWebpackPlugin()]
 });
 
